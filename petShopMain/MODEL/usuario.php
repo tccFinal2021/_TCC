796f238bd0e484b1841c2c1746b7ff0267d669b7
tccFinal2021 <77889544+tccFinal2021@users.noreply.github.com>
2021-06-20 15:05:38 -0300

    Add files via upload

--- petShopMain/MODEL/usuario.php
+++ petShopMain/MODEL/usuario.php
@@ -1,159 +1,146 @@
 <?php
 
-class Usuario{
-    private $id;
-    private $nome;
-    private $sobrenome;
-    private $dataNascimento;
-    private $email;
-    private $senha;
+if(!isset($_SESSION))
+{ 
+    session_start(); 
+} 
 
-
-//id
-public function setID($id){
-    $this ->id = $id;
-}
-public function getID(){
-    return $this -> id;
-}
-//nome
-public function setNome($nome){
-    $this -> nome = $nome;
-}
-public function getNome(){
-    return $this -> nome;
-}
-//cpf
-public function setSobrenome($sobrenome){
-    $this -> sobrenome = $sobrenome;
-}
-public function getSobrenome(){
-    return $this -> sobrenome;
-}
-//email
-public function setEmail($email){
-    $this -> email= $email;
-}
-public function getEmail(){
-    return $this -> email;
-}
-//dataNascimento
-public function setDataNascimento($dataNascimento){
-    $this ->dataNascimento =  $dataNascimento;
-}
-public function getDataNascimento(){
-    return $this -> dataNascimento;
-}
-//senha
-public function setSenha($senha){
-    $this ->senha = $senha;
-}
-public function getSenha(){
-    return $this -> senha;
+    class Usuario{
+        private $id;
+        private $nome;
+        private $sobrenome;
+        private $email;
+        private $senha;
     
-}
 
-public function inserirBD(){
-    require_once 'ConexaoBD.php';
-
-    $con = new ConexaoBD();
-    $conn = $con->conectar();
-    if ($conn-> connect_error){
-        die ("conection failed: ". $conn->connect_error); 
-    }
-    $sql = "INSERT INTO usuarioteste (nome, sobrenome, datanasc, email, senha)
-    VALUES ('".$this->nome."', '".$this->sobrenome."', '".$this->dataNascimento."','".$this->email."', '".$this->senha."')";
-
-    if ($conn->query($sql) === TRUE) {
-        $this ->id = mysqli_insert_id($conn);
-        $conn -> close();
-        return TRUE;
-    } else {
-        $conn->close();
-        return FALSE;
+      
+        
+        //id
+        public function setID($id){
+            $this ->id = $id;
+        }
+        public function getID(){
+            return $this -> id;
+        }
+        //nome
+        public function setNome($nome){
+            $this ->nome = $nome;
+        }
+        public function getNome(){
+            return $this -> nome;
+        }
+        //Sobrenome
+        public function setSobrenome($sobrenome){
+            $this ->sobrenome = $sobrenome;
+        }
+        public function getSobrenome(){
+            return $this -> sobrenome;
+        }
+        //email
+        public function setEmail($email){
+            $this ->email = $email;
+        }
+        public function getEmail(){
+            return $this -> email;
         }
 
-    }
-    
- public function listaUsuarios()
- {
- require_once 'ConexaoBD.php'; 
- 
- $con = new ConexaoBD();
- $conn = $con->conectar();
- if ($conn->connect_error) {
- die("Connection failed: " . $conn->connect_error);
- } 
- $sql = "SELECT * FROM usuarioteste;" ;
- $re = $conn->query($sql);
- $conn->close();
- return $re;
- }
-
-    public function carregarUsuario (){
-        require_once 'ConexaoBD.php';
-
-        $con = new ConexaoBD();
-        $conn = $con->conectar();
-        if ($conn->connect_error){
-            die("conection failed: ".$conn->connect_error);
+        //senha
+        public function setSenha($senha){
+            $this ->senha = $senha;
         }
-        $sql = "SELECT * FROM usuario WHERE email = ".$email; 
-        $re =  $conn->query($sql);
-        $r = $re->fetch_object();
-        if ($r != null){
-            $this->id = $r->idusuario;
-            $this->nome = $r->nome;
-            $this->email = $r-> email;
-            $this ->sobrenome = $r-> sobrenome;
-            $this ->dataNascimento = $r->DataNascimento;
-            $this ->senha = $r->senha;
-            $conn->close();
-            return true;
+        public function getSenha(){
+            return $this -> senha;
+            
+        }
+
+       
+
+        
+        
+
+        //função de inserção de dados no banco
+        public function inserirBD(){
+
+            //inserindo arq de conexão ao banco
+            require_once 'ConexaoBD.php';
+            //verificando 
+            $con = new ConexaoBD();
+            $conn = $con->conectar();
+            if ($conn-> connect_error){
+                die ("Falha na Conexão: ". $conn->connect_error); 
+            }
+            
+            //inserindo dados na tabela usuario
+            $sql = "INSERT INTO usuario (nome, sobrenome, email, senha)
+            VALUES ('".$this->nome."', '".$this->sobrenome."', '".$this->email."', '".$this->senha."')";
+        
+            if ($conn->query($sql) === TRUE) {
+                $this ->id = mysqli_insert_id($conn);
+                $conn -> close();
+                return TRUE;
             } else {
                 $conn->close();
-                return false;
-            }
-    }
-
-    public function atualizarBD(){
-        require_once 'conexaoBD.php';
-        $con = new ConexaoBD();
-        $conn = $con->conectar();
-        if ($conn->connect_error){
-            die("conection failed: ". $conn->connect_error);
-
-        }
-        $sql = "UPDATE usuarioteste SET nome = '".$this->nome."', sobrenome ='".$this->sobrenome."', datanasc = '".$this->dataNascimento."', email= '".$this->email."' WHERE email = '".$this->email."'" ;
-        if ($conn->query($sql) === TRUE) {
-            $conn -> close();
-            return TRUE;
-        } else {
-            $conn-> close();
-            return FALSE;
-        }
-    }
-
-    public function excluirBD($email){
-        require_once 'ConexaoBD.php';
-        $con = new ConexaoBD();
-        $conn = $con->conectar();
+                return FALSE;
+                }
         
-        if ($conn->connect_error){
-            die("conection failed: ". $conn->connect_error);
-        }
+            }
+            //Funçao de busca dados no bd para validar o login
+            public function logarBD(){
+                
+                require_once 'ConexaoBD.php';
+        
+                $con = new ConexaoBD();
+                $conn = $con->conectar();
+                if ($conn->connect_error){
+                    die("conection failed: ".$conn->connect_error);
+                }
+                $sql = "SELECT * FROM usuario WHERE email = '".$this->email."';";
+                $re =  $conn->query($sql);
+                //echo sql
+                $linha = mysqli_fetch_array($re);
+                if ($linha != null)
+                {
+                    if($linha['senha'] == $this->senha)
+                    {
+                        $_SESSION['logado'] = $this->email;
+                        $_SESSION['nome'] = $linha['nome'];
+                        return TRUE;
+                        
+                    }   
+                else 
+                {
+                        return FALSE;
+                        $conn->close();
+                }
+                }
+            }
 
-        $sql = "DELETE FROM usuarioteste where email = '".$email."';";
-        if ($conn->query($sql) === TRUE) {
-            $conn -> close();
-            return TRUE;
-        } else {
-            $conn-> close();
-            return FALSE;
-        }
+            //alteraçao de dados
+            public function atualizarBD(){
+                require_once 'conexaoBD.php';
+                $con = new ConexaoBD();
+                $conn = $con->conectar();
+                if ($conn->connect_error){
+                    die("conection failed: ". $conn->connect_error);
+        
+                }
+                $sql = "UPDATE usuario SET nome = '".$this->nome."', sobrenome ='".$this->sobrenome."', 
+                /*dataNascimento = 
+                '".$this->dataNascimento."',
+                */
+                 email= '".$this->email."' WHERE idusuario = '".$this->id."'" ;
+                if ($conn->query($sql) === TRUE) {
+                    $conn -> close();
+                    return TRUE;
+                } else {
+                    $conn-> close();
+                    return FALSE;
+                }
+            }
+
+        
+
+    //fechamento da classe
     }
 
-
- 
-}
 ?>
